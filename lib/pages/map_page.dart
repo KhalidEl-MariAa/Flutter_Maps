857bdf83922d546364194ca899b7e5dda63c835b
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-09-28 17:38:48 +0200

    map page sa stateful widget

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -10,6 +10,6 @@ class MapPage extends StatefulWidget {
 class _MapPageState extends State<MapPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
